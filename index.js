0ed973f68cb6d460f766f7daecdce9261fbe3ee5
zalfanuramalia <zalfafanur@gmail.com>
2022-05-18 08:13:00 +0700

    chore: change port

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(cors())
 app.use(require('./src/routes'))
 app.use('/uploads', express.static('uploads'))
 
-const APP_PORT = process.env.PORT || 3000
-
-app.listen(APP_PORT, () => {
-  console.log(`App running on port ${APP_PORT}`)
+app.listen(8080, () => {
+  console.log('App listening on port 8080')
 })
